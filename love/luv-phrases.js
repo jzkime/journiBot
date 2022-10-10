cb4977302f77622346f064e4c255880e898172f2
Jaz Kime <JazDKime@gmail.com>
2022-10-10 11:49:29 -0700

    remove love dummy data

--- love/luv-phrases.js
+++
@@ -1,5 +0,0 @@
-module.exports = [
-    'i love you to the moon and back',
-    'you are my sunshine',
-    'spending time with you makes the world worth experiencing'
-]
--- love/select-phrase.js
+++
@@ -1,8 +0,0 @@
-const luv = require('./luv-phrases');
-
-function sendPhrase() {
-    const num = Math.floor(Math.random() * luv.length);
-    return luv[num];
-}
-
-module.exports = sendPhrase();
