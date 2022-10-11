2565878514ae4fa7ff9ba0fc94faab3b73bd0a69
Jaz Kime <JazDKime@gmail.com>
2022-10-11 14:44:01 -0700

    changed db column to be discord_id to better find messages

--- data/migrations/20221011203650_create-messages.js
+++ data/migrations/20221011203650_create-messages.js
@@ -1,7 +1,7 @@
 exports.up = function(knex) {
   return knex.schema.createTable('users', tbl => {
     tbl.increments('user_id');
-    tbl.string('username');
+    tbl.string('discord_id');
   }).createTable('messages', tbl => {
     tbl.increments('message_id');
     tbl.string('user_id')
