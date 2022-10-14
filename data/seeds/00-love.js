b4a167d158c3e1b1ecadb857f8c9dc303c83915d
Jaz Kime <JazDKime@gmail.com>
2022-10-14 14:42:45 -0700

    changed knex love seed message

--- data/seeds/00-love.js
+++ data/seeds/00-love.js
@@ -2,7 +2,7 @@ exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('love').truncate()
   await knex('love').insert([
-    {love_id: 1, love_message: 'youoouyoyodyoyoy'},
+    {love_id: 1, love_message: 'yooooooo'},
     {love_id: 2, love_message: 'hello world'},
     {love_id: 3, love_message: 'uwu'},
     {love_id: 4, love_message: 'i love you to the moon and back'},
